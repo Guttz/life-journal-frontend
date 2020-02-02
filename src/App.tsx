953b666ed4ec1cee119f9100dd6004ab53089ac4
Gustavo Morais <gustavopguto@gmail.com>
2020-02-02 19:22:47 -0200

    Typescript setup, with reactstrap and react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import Page404 from './views/Pages/Page404/Page404';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
+    <HashRouter>
+      <React.Suspense fallback={loading()}>
+        <Switch>
+          <Route exact path="/404" render={props => <Page404 />} />
+          <Route
+            path="/"
+            render={props => (
+              <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                  </p>
+                  <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            )}
+          />
+        </Switch>
+      </React.Suspense>
+    </HashRouter>
+
+    /*     <div className="App">
+      <Page404></Page404>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
-    </div>
+    </div> */
   );
-}
+};
 
 export default App;
