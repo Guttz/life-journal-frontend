70777daadfd0b53e3d9131956ae041314f0f9c14
Gustavo Morais <gustavopguto@gmail.com>
2020-02-03 21:12:26 -0200

    Removing unused code

--- src/App.tsx
+++ src/App.tsx
@@ -31,19 +31,6 @@ const App: React.FC = () => {
         </Switch>
       </React.Suspense>
     </HashRouter>
-
-    /*     <div className="App">
-      <Page404></Page404>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div> */
   );
 };
 
