137b4e7e8ff9002de1fb57afd10253e4c3a72a83
Gustavo Morais <gustavopguto@gmail.com>
2020-03-13 13:12:45 -0300

    Adding timeline container

---
+++ src/containers/Timeline.tsx
@@ -0,0 +1,17 @@
+import Timeline from './../views/Home/Home';
+import { connect } from 'react-redux';
+import actions from './../store/actions';
+
+const mapStateToProps = (state: any): any => {
+  return {
+    items: state.items,
+  };
+};
+
+// Question - Best way to mapDipatchToProps, dispatch here or in view
+const mapDispatchToProps = (dispatch: any): any => ({
+  createItem: (description: string): void => dispatch(actions.createItem(description)),
+  deleteItem: (id: number): void => dispatch(actions.deleteItem(id)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timeline);
