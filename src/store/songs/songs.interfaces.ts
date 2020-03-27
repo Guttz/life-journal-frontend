88077b793808a13f684824d29631eaaad7d61648
Gustavo Morais <gustavopguto@gmail.com>
2020-03-27 20:22:26 -0300

    Adding songs interfaces

---
+++ src/store/songs/songs.interfaces.ts
@@ -0,0 +1,15 @@
+export type SongInterface = {
+  id: number;
+  name: string;
+  artists: Array<string>;
+  previewURL: string;
+  imageURL: string;
+  importance?: number;
+  x: number;
+  y: number;
+};
+
+export type SongsInterface = {
+  lastIndex: number;
+  songs: Array<SongInterface>;
+};
