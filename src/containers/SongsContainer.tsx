e2aea5717618af9fc36fcd8ca23b00d27b023d70
Gustavo Morais <gustavopguto@gmail.com>
2020-03-27 20:28:07 -0300

    Creating SongsContainer

---
+++ src/containers/SongsContainer.tsx
@@ -0,0 +1,27 @@
+import Timeline from '../views/Home/Home';
+import { connect, ConnectedProps } from 'react-redux';
+import { Dispatch, AnyAction } from 'redux';
+import { RootState } from '../store/rootState';
+import { SongsActionsTypes } from '../store/songs/songs.actions.types';
+import { SongInterface } from '../store/songs/songs.interfaces';
+import SongActions from '../store/songs/songs.actions';
+
+const mapStateToProps = (state: RootState): any => {
+  return {
+    lastIndex: state.moments.lastIndex,
+    songs: state.moments.songs,
+  };
+};
+
+// Question - Best way to mapDipatchToProps, dispatch here or in view
+const mapDispatchToProps = (dispatch: Dispatch<SongsActionsTypes>): any => ({
+  insertSong: (song: SongInterface): any => dispatch(SongActions.InsertSong(song)),
+});
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+// The inferred type will look like:
+// {isOn: boolean, toggleOn: () => void}
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timeline);
