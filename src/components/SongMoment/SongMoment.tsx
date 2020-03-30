c289b5dadfaa46ef79eeb25d51b6d25dbd30e615
Gustavo Morais <gustavopguto@gmail.com>
2020-03-29 21:18:12 -0300

    Song Moment updating its state

--- src/components/SongMoment/SongMoment.tsx
+++ src/components/SongMoment/SongMoment.tsx
@@ -1,43 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { Text, Image, Ring, Circle, Line, Group } from 'react-konva';
+import Konva from 'konva';
 import useImage from 'use-image';
 
 type Props = {
   imgURL: string;
-  text: string;
+  name: string;
   x: number;
   y: number;
   scaleX: number;
   scaleY: number;
   onDragStart(event: any): void;
+  onDragMove(event: any): void;
   onDragEnd(event: any): void;
   onDblClick(event: any): void;
   createItem?(task: string): any;
 };
 
 // eslint-disable-next-line react/prop-types
-const SongMoment: React.FC<Props> = ({ imgURL, text, x, y, scaleX, scaleY, onDragStart, onDragEnd, onDblClick }) => {
+const SongMoment: React.FC<Props> = ({
+  imgURL,
+  name,
+  x,
+  y,
+  scaleX,
+  scaleY,
+  onDragStart,
+  onDragMove,
+  onDragEnd,
+  onDblClick,
+}) => {
   const [image] = useImage(imgURL);
+
   return (
     <Group
       absolutePosition={{ x: x, y: y }}
       onDragStart={onDragStart}
+      onDragMove={onDragMove}
       onDragEnd={onDragEnd}
       onDblClick={onDblClick}
       draggable
       scaleX={scaleX}
-      scaleY={scaleY}>
-      <Line points={[window.innerWidth / 2 - 100, y - 66, x + 80, y - 66]} stroke="grey" />
+      scaleY={scaleY}
+    >
+      <Line points={[window.innerWidth / 2 - x, 33, 80, 33]} stroke="grey" />
       <Image onClick={() => {}} width={66} height={66} scaleY={1} image={image} />
       <Ring x={33} y={33} innerRadius={35} outerRadius={47} fill="#333333"></Ring>
       <Circle x={33} y={33} radius={35} stroke="#26A65B" strokeWidth={2}></Circle>
-      <Text
-        text={text}
-        fill="white"
-        x={86}
-        y={24}
-
-      />
+      <Text text={name} fill="white" x={86} y={24} />
     </Group>
   );
 };
