66fb03996481932ee2d1ade645bacd19fb9aa011
Gustavo Morais <gustavopguto@gmail.com>
2020-03-29 21:17:18 -0300

    Saving Redux State to locahost cache

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { saveState, loadState } from './../utils/localStorage';
 
 // Logger with default options
 import logger from 'redux-logger';
@@ -8,6 +9,17 @@ import { rootReducer } from './rootState';
 export type RootState = ReturnType<typeof rootReducer>;
 
 export default function configureStore() {
-  const store = createStore(rootReducer, applyMiddleware(logger));
+  const persistedState = loadState();
+  const store = createStore(rootReducer, persistedState, applyMiddleware(logger));
+
+  let throttleTime = new Date().getTime();
+
+  store.subscribe(() => {
+    if (new Date().getTime() - throttleTime > 1000) {
+      throttleTime = new Date().getTime();
+      saveState(store.getState());
+    }
+  });
+
   return store;
 }
